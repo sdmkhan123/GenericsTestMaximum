2e603d2af19be37671bef5ab9754dea04e5e1dd2
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-16 16:08:51 +0530

    Welcome to Generics Test Maximum

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsTestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Test Maximum");
+        }
+    }
+}
