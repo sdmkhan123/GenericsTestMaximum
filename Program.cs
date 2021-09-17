c546b1890b0ce3f034ab59b5d9597b225f7c5be3
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-17 17:32:51 +0530

    Find Max int Out Of Three int

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenericsTestMaximum.FindMaxOutOfThree;
+using System;
 
 namespace GenericsTestMaximum
 {
@@ -6,7 +7,29 @@ namespace GenericsTestMaximum
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***************************************************************");
             Console.WriteLine("Welcome to Generics Test Maximum");
+            Console.WriteLine("***************************************************************");
+            while (true)
+            {
+                Console.WriteLine("Press 1 : to comapre 3 int\nPress 8 : to exit");
+                //Press 2 : to comparre 3 float\nPress 3 : to compare 3 string
+                Console.WriteLine("***************************************************************");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        int num = MaxOutOfThree.maxIntNumber();
+                        Console.WriteLine("max int out of 3 int is :--> " + num);
+                        break;
+                    case 8:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("***************************************************************");
+            }
         }
     }
 }
